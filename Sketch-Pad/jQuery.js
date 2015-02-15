ec9b2975036e390b6d6391efeb8663d9bdb094f7
bon22801 <bon22801@gmail.com>
2015-02-14 21:02:53 -0500

    implemented new pad and shows size on top

--- Sketch-Pad/jQuery.js
+++ Sketch-Pad/jQuery.js
@@ -1,12 +1,38 @@
 $(document).ready(function(){
-	for (i=0; i<16; i++){
-		for(j=0;j<16;j++){
+	$("body").append('<button type="button"> New Pad </button>');
+	$("body").append('<div class="container"></div>');
+
+	init_grid(16);
+	hover();
+
+	$("button").on("click", function() {
+		var size = parseInt(prompt("Enter a new pad size:"));
+		$(".grid").remove();
+		$("#size").remove();
+		init_grid(size);
+		hover();
+	});
+
+
+});
+
+
+
+
+
+function init_grid(size){
+	$("body").prepend('<h1 id="size">' + size + 'x' + size +' grids</h1>');
+	for (i=0; i<size; i++){
+		for(j=0;j<size;j++){
 			$(".container").append('<div class ="grid"></div>');
 		}
 		$(".container").append('<div class ="new_row"></div>');
 	}
+}
 
+function hover(){
 	$(".grid").mouseenter(function(){
-		$(this).addClass("gird-hovered")
-	})
-});
+	$(this).addClass("gird-hovered")
+	});
+}
+
