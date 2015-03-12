69168ab1ec1a786400346c72a58ce94b09d224dc
bon22801 <bon22801@gmail.com>
2015-03-11 23:38:25 -0400

    Sketch-Pad: add colors option and fixed NA grids bug

--- Sketch-Pad/jQuery.js
+++ Sketch-Pad/jQuery.js
@@ -1,11 +1,19 @@
+var color = "black";
+
 $(document).ready(function(){
-	$("body").append('<button type="button"> New Pad </button>');
+	$("body").append('<button id="newPad" type="button"> New Pad </button>');
+	$("body").append('<div class="colors"></div>');
+	$(".colors").append('<button id="red" type="button">  </button>');
+	$(".colors").append('<button id="blue" type="button">  </button>');
+	$(".colors").append('<button id="green" type="button">  </button>');
+	$(".colors").append('<button id="white" type="button">  </button>');
+	$(".colors").append('<button id="black" type="button">  </button>');
 	$("body").append('<div class="container"></div>');
 
 	init_grid(16);
 	hover();
 
-	$("button").on("click", function() {
+	$("#newPad").on("click", function() {
 		var size = parseInt(prompt("Enter a new pad size:"));
 		$(".grid").remove();
 		$("#size").remove();
@@ -13,7 +21,22 @@ $(document).ready(function(){
 		hover();
 	});
 
+	$("#red").on("click", function() {
+		color = "red";
+	});
 
+	$("#blue").on("click", function() {
+		color = "blue";
+	});
+	$("#green").on("click", function() {
+		color = "green";
+	});
+	$('#white').on("click", function() {
+		color = "white";
+	});
+	$('#black').on("click", function() {
+		color = "black";
+	});
 });
 
 
@@ -21,6 +44,7 @@ $(document).ready(function(){
 
 
 function init_grid(size){
+	if (size < 0 || isNaN(size)){size = 0};
 	$("body").prepend('<h1 id="size">' + size + 'x' + size +' grids</h1>');
 	for (i=0; i<size; i++){
 		for(j=0;j<size;j++){
@@ -32,7 +56,7 @@ function init_grid(size){
 
 function hover(){
 	$(".grid").mouseenter(function(){
-	$(this).addClass("gird-hovered")
+	$(this).addClass(color)
 	});
 }
 
